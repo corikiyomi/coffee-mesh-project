14820f50f34073ee0235b158e5a439c0657f8394
Cori Komiyama <corikiyomi@gmail.com>
2024-03-21 08:53:52 -0700

    Deleted redundant ch06/orders folder

---
+++ ch06/orders/api/schemas.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel, Field, conlist, conint, validator
+
+class Size(Enum):   # We declare an enumeration schema
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+class Status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+class OrderItemSchema(BaseModel):   # Every pydantic model inherits from pydantic's BaseModel
+    product: str    # We use Python-type hints to specify the type of attribute
+    size: Size      # We constrain the values of a property by setting its type to enumeration
+    quantity: Optional[conint(ge=1, strict=True)] = 1   # We specify quantity's minimum value and we give it a default
+    
+    @validator('quantity')
+    def quantity_non_nullable(cls, value):
+        assert value is not None, 'quantity may not be None'
+        return value
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_length=1)    # We use pydantic's conlist type to define a list with at least one element
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
